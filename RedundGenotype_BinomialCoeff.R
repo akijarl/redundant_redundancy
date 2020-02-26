2c8c73f5f3dd24ffc07c951bd38e5b5e16f70182
Áki Jarl Láruson <akijarl@gmail.com>
2020-02-26 20:22:25 +0000

    Updated binomial coeff. code

--- RedundGenotype_BinomialCoeff.R
+++ RedundGenotype_BinomialCoeff.R
@@ -6,76 +6,55 @@
 library(gtools)
 library(dplyr)
 
-# could have 1 to 10 mutations evolve
-# need to loop through all possible mutational states "r" from 1 to 10
-# each one is a different size matrix
-# idea: collapse genotype into single character with it's phenotype value
+### Function to calculate Redundancy for `n=10` genotypes - NUMERICALLY ####
 
-genotype <- c()
-phenotype <- c()
-
-for (r in 1:nrow(all_loci)){
-  print(r)
-  combo.df <- data.frame(combinations(n=length(all_loci$names), r=r, 
-                                      all_loci$names, repeats.allowed = FALSE),
-                         stringsAsFactors = FALSE)
-  
-  head(combo.df)
-  colnames(combo.df) <- paste0("A", 1:ncol(combo.df))
-  
-  combo.df2 <- combo.df
-  for (i in 1:ncol(combo.df)){
-    combo.df[,i][grep("n", combo.df[,i], perl=TRUE)] <- -alpha
-    # get indexes with n and replace with minus alpha
-    combo.df[,i][grep("p", combo.df[,i], perl=TRUE)] <- alpha
-    combo.df[,i] <- as.numeric(combo.df[,i])
-  }
-  
-  genotype <- c(genotype, apply(combo.df2 , 1 , paste , collapse = "_" ))
-  phenotype <- c(phenotype, rowSums(combo.df))
+redundancy.numerical <- function(nloci, alpha) {
+		
+	np <- round(nloci/2,0) #number of mutations with positive effect sizes
+	nn <- np #number of mutations with negative effect sizes
+	
+	p_names <- paste0("p", 1:np)
+	n_names <- paste0("n", 1:nn)
+	
+	all_loci <- data.frame(names=(c(p_names, n_names)), 
+	                       alpha = c(rep(alpha, length(p_names)),
+	                                 rep(-alpha, length(n_names))))
+	all_loci$names <- as.character(all_loci$names)
+	
+	# could have 1 to 10 mutations evolve
+	# need to loop through all possible mutational states "r" from 1 to 10
+	# each one is a different size matrix
+	# idea: collapse genotype into single character with it's phenotype value
+	
+	genotype <- c()
+	phenotype <- c()
+	
+	for (r in 1:nrow(all_loci)){
+	  print(r)
+	  combo.df <- data.frame(combinations(n=length(all_loci$names), r=r, 
+	                           all_loci$names, repeats.allowed = FALSE),
+	                         stringsAsFactors = FALSE)
+	  
+	  head(combo.df)
+	  colnames(combo.df) <- paste0("A", 1:ncol(combo.df))
+	  
+	  combo.df2 <- combo.df
+	  for (i in 1:ncol(combo.df)){
+	    combo.df[,i][grep("n", combo.df[,i], perl=TRUE)] <- -alpha
+	      # get indexes with n and replace with minus alpha
+	    combo.df[,i][grep("p", combo.df[,i], perl=TRUE)] <- alpha
+	    combo.df[,i] <- as.numeric(combo.df[,i])
+	  }
+	  
+	  
+	  genotype <- c(genotype, apply( combo.df2 , 1 , paste , collapse = "_" ))
+	  phenotype <- c(phenotype, rowSums(combo.df))
+	}
+	final.df <- data.frame(genotype, phenotype)
+	return(final.df)
 }
 
-final.df <- data.frame(genotype, phenotype)
-head(final.df, 100)
-tail(final.df, 100)
-
-# Plot example of fitness as a function of phenotype ####
-# stabilizing selection
-
-#pdf("RedundancyForOptimum_10genotype.pdf", width=5, height=4)  
-par(mar=c(4,3,1,1))
-
-x2<- seq(0,2,0.001)
-y2 <- dnorm(x2, mean = 1, sd=0.5/10)
-y2_scaled <- y2/max(y2)
-
-plot(c(x2, x2[1]), c(y2_scaled, y2_scaled[1]), 
-     ylim=c(-1,1.3), bty="n", yaxt="n",type="l",
-     xlim=c(-2,2), xaxs="i", ylab="", xlab="Phenotype",
-     col=rgb(0,0.4, 0.8))
-
-polygon(c(x2, x2[1]), c(y2_scaled, y2_scaled[1]), 
-        col=rgb(0,0.4, 0.8))
-
-
-mtext("Density", side=2, line=1)
-
-text(0, 0.5, "Fitness\nfunction", col=rgb(0,0.4, 0.8))
-abline(h=0, lwd=1)
-
-# Plot redundancy as a function of phenotype
-# number of ways to get phenotype over evolutionary timescales
-y1 <- hist(phenotype, breaks=seq(-alpha*nn-alpha/2,alpha*nn+alpha/2, alpha), plot=FALSE)
-(y1df <- cbind(y1$mids, y1$counts))
-y1_scaled =  y1$density/max(y1$density)
-points(y1$mids, -y1_scaled, type="h", col=rgb(1,0.5,0.5), lwd=1)
-
-text(0.7, -0.5, "Genetic\nredundancy", col=rgb(1,0.5,0.5))
-
-#dev.off()
-# end plot ####
-
-y1df
+### Function to calculate Redundancy for `n=10` genotypes - BINOMIAL COEFFICIENT ####
 
 redundancy <- function(nloci, alpha) {
   # assume equal number of + loci and - loci
@@ -84,5 +63,22 @@ redundancy <- function(nloci, alpha) {
   return(data.frame(phen, Num, log10Num=log10(Num)))
 }
 
-redundancy(10, 0.2)
-redundancy(40, 0.2)
+
+alpha = 0.2
+n=10
+final.df.num <- redundancy.numerical(n, alpha)
+hist(final.df.num$phenotype, main = "Numerical number of genotypes for each phenotype", xlab="Phenotype", breaks=seq(-(n/2)*alpha-alpha/2, (n/2)*alpha+alpha/2, by=alpha))
+
+
+final.df.binom <- redundancy(n, alpha)
+points(final.df.binom$phen, final.df.binom$Num, col="blue")
+
+alpha = 0.3
+n=20
+final.df.num <- redundancy.numerical(n, alpha)
+hist(final.df.num$phenotype, main = "Numerical number of genotypes for each phenotype", xlab="Phenotype", breaks=seq(-(n/2)*alpha-alpha/2, (n/2)*alpha+alpha/2, by=alpha))
+
+
+final.df.binom <- redundancy(n, alpha)
+points(final.df.binom$phen, final.df.binom$Num, col="blue")
+
